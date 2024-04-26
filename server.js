6cc957f838b84a9773155cd4ebbc99ebd17f949a
Kumaran <senthuklms@gmail.com>
2024-04-26 23:48:23 +0530

    updating hello world server.js

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
+// Import required modules
 const express = require('express');
+// Create an Express app
 const app = express();
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-// const url = 'https://fmcna.com/insights/articles/5-diamond-status-kathleen-belmonte/'; // Replace this with the URL you want to scrape
-const port = process.env.PORT || 3000;
-
-// Sample API endpoint
-app.get('/scrape', (req, res) => {
-    axios.get(req.query.url)
-    .then(response => {
-        // const html = response.data;
-        // const $ = cheerio.load(html);
-
-        // // Your scraping logic here
-        // // Example: Get the title of the page
-        // const pageTitle = $('.h1-style .text-dark-blue').text();
-        // console.log('Page Title:', pageTitle);
-        res.json({ pageHeading: "pageTitle" });
-    })
-    .catch(error => {
-        console.log('Error fetching the page:', error);
-    });
-  
+// Define a sample API endpoint
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express API!' });
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
