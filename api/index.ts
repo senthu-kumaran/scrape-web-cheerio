d12c54525b6dbd4fdbc947476d8c0edf6d13c19b
Kumaran <senthuklms@gmail.com>
2024-04-27 00:31:16 +0530

    Using api route

---
+++ api/index.ts
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+app.get("/scrape", (req, res) => {
+    axios.get(req.query.url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        // Your scraping logic here
+        // Example: Get the title of the page
+        const pageTitle = $('.h1-style .text-dark-blue').text();
+        console.log('Page Title:', pageTitle);
+        res.json({ pageHeading: pageTitle });
+    })
+    .catch(error => {
+        console.log('Error fetching the page:', error);
+    });
+});
+
+app.listen(9001, () => console.log("Server ready on port 9001."));
+
+module.exports = app;
